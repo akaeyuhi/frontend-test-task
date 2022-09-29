a737f91c040bc98255fd64673740d0bd27faa267
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-09-29 15:36:42 +0300

    MainBody basic code

---
+++ src/Components/MainBody.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function MainBody() {
+    return <div className="main-body">
+        <h2>Main body</h2>
+        <button className="click-button">Change colors</button>
+    </div>
+}
