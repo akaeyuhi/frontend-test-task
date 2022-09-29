165050dcff46a3db503b8cf895da9972a268efe2
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-09-29 15:35:26 +0300

    Fixed tag names

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Footer() {
-    return <header className="footer">
+    return <footer className="footer">
         <h1>Test Task Footer</h1>
-    </header>
+    </footer>
 }
--- src/Components/LeftPanel.tsx
+++ src/Components/LeftPanel.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function LeftPanel() {
-    return <header className="left-panel">
+    return <div className="left-panel">
         <p>Left Panel</p>
-    </header>
+    </div>
 }
