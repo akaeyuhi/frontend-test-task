2360b203bc142701814ad224c7790d717ba5147d
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-09-29 15:41:13 +0300

    Add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from './Components/Header';
+import LeftPanel from "./Components/LeftPanel";
+import RightPanel from "./Components/RightPanel";
+import Footer from "./Components/Footer";
+import MainBody from "./Components/MainBody";
 
 function App() {
   return (
     <div className="App">
-
+        <Header />
+        <LeftPanel/>
+        <MainBody/>
+        <RightPanel/>
+        <Footer/>
     </div>
   );
 }
